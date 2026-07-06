2fe9bf7a0b76de24020ae05bf66e485cfa376232
Heena Shaikh <heena.s.shaikh.09@gmail.com>
2026-07-06 21:28:30 +0100

    boto3 code for storing the local file to S3

--- pandas/ex.py
+++ pandas/ex.py
@@ -1,7 +1,24 @@
 import pandas as pd
+import boto3 
+
+
 
 BUCKET = "s3-heena-demo-session3"
-df = pd.read_csv("s3://s3-heena-demo-session3/sales2.csv") 
 
-print (df)
-print(df.head())
+#pd.set_option("display.max_columns", None)                         //displays all the columns         
+#df = pd.read_csv("s3://s3-heena-demo-session3/sales2.csv") 
+
+#To display only columns on Panda
+#print(df.columns)
+#print(df.info())
+
+#Print first 5 rows of the dataframe
+#print(df.head())
+
+
+s3 = boto3.client("s3")
+s3.upload_file(
+    Filename = "erp_data/sales.csv",
+    Bucket = "s3-heena-demo-session3",
+    Key = "erp_data/sales.csv"
+)
